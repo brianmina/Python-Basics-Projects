9686a7f9bc71f8b303a222adb91e342c9731c02b
brianmina <brian.mina17@gmail.com>
2024-11-11 04:30:45 -0500

    new function added "mark as complete, and pending to fix itetation

--- daily_task_tracker/task_tracker.py
+++ daily_task_tracker/task_tracker.py
@@ -4,7 +4,7 @@ tasks = []
 
 
 def add_task(task):
-    tasks.append({"task": task, "complete": False})
+    tasks.append({"task": task, "completed": False})
 
 
 def complete_tast(task_name):
@@ -15,6 +15,48 @@ def complete_tast(task_name):
             return
     print(f"Task '{task_name}' not found.")
 
+#TODO: create 2 functions(view_tasks() and incomplete_tasks())
+
+def view_tasks():
+    print("\nYour task:")
+    for task in tasks:
+        status = "Completed" if task["completed"] else "Imcomplete"
+        print(f"- {task['task']}: {status}")
+    print()
+
+
+def incomplete_tasks():
+    print("\nIncomplete tasks:")
+    for task in tasks:
+        if not task["completed"]:
+            print(f"-{task['task']}")
+    print()
+
+
+def delete_task(task_name):
+    # This function will allow the user to remove a specific task by name.
+    for index, task in enumerate(tasks):
+        if task['task'] == task_name:
+            del tasks[index]
+            print(f"Task '{task_name}' has been deleted.")
+            break
+        else:
+            print(" hello")
+            print(f"Task '{task_name}' not found.")
+
+
+def mark_as_complete(task_name):
+    #This task allows you to mark a task as complete if it exists in the task list
+    for task in tasks:
+        if task['task'] == task_name:
+            task['task'] = "complete"
+            print(f"Task '{task_name}' has been marked as complete.")
+            break
+    else:
+        print(f"Task '{task_name}' not found in the task list")
+
+
+
 
 add_task("Do the laundry")
 add_task("Read a book")
@@ -22,5 +64,10 @@ add_task("Do yoga")
 add_task("Go to the gym")
 add_task("Study programming")
 
+view_tasks()
+incomplete_tasks()
 
-print(tasks)
+delete_task("Go to the gym")
+mark_as_complete("Study programming")
+
+#TODO: create a interactive menu 
