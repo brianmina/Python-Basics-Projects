be5d233d2089fd0d8c9f2fd3c8d14534a5a77509
brianmina <brian.mina17@gmail.com>
2024-11-14 16:07:32 -0500

    new project intiated

---
+++ budget_calculator/budget_calculator.py
@@ -0,0 +1,12 @@
+
+
+expenses = []
+
+def menu():
+    print("Welcome to the Expense Tracker!")
+    while True:  # Keeps the program running in a loop
+        print("\nPlease choose an option:")
+        print("1. Add an expense")        
+        print("2. View all expenses")      
+        print("3. Calculate total expenses") 
+        print("4. Exit")    
